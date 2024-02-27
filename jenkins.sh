b041e82768813d7b1644567867602a2370ccab4c
Ubuntu <ubuntu@ip-120-0-1-134.ap-south-1.compute.internal>
2024-02-27 08:35:23 +0000

    Jenkins Installation Script file with docker & kubernetes

--- jenkins.sh
+++ jenkins.sh
@@ -3,6 +3,22 @@ sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
     https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
 sudo apt-get install openjdk-11-jre -y
 sudo apt-get install jenkins -y
+sudo apt-get install docker.io -y
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -y
+sudo apt-get install kubectl
+sudo apt-get upgrade -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
+git --version
+java --version
+jenkins --version
+docker --version
+kubectl version --output=yaml
