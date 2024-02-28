0dd72183692dd709eccffa1f4107f260dd09b9cd
Ubuntu <ubuntu@ip-120-0-1-229.ap-south-1.compute.internal>
2024-02-28 07:40:16 +0000

    updated file of kubernetes

--- kubectl.sh
+++ kubectl.sh
@@ -3,11 +3,20 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -y
-sudo apt-get install docker.io
+sudo apt-get upgrade -y
 sudo apt-get install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
-sudo systemctl start docker
-sudo systemctl enable docker
-docker --version
 kubectl version --short
 kubeadm version --output=yaml
+echo ("Docker checking and installation starting")
+count=$(apt list --installed | grep docker | wc -l)
+if [ $count -eq 1 ]
+then
+        docker --version
+else
+        sudo apt-get install docker.io -y
+	sudo systemctl start docker
+	sudo systemctl enable docker
+fi
+docker --version
+sudo docker info
 
