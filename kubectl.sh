4aa28b3d5ef73de263ef492404829b9e2d6a7bcd
U-NEXSUS\Ravi.Sharma <Ravi.Sharma@Nexsus-TechLap3.nexsus.local>
2024-02-28 11:59:30 +0530

    Executable file for the installation of Jenkins,docker and kubernetes

--- kubectl.sh
+++ kubectl.sh
@@ -3,4 +3,11 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -y
+sudo apt-get install docker.io
 sudo apt-get install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
+kubectl version --short
+kubeadm version --output=yaml
+
