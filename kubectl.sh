09ae5e6ddb9c6eb70aa94fa5a7df19b86e87695c
Ubuntu <ubuntu@ip-120-0-1-156.ap-south-1.compute.internal>
2024-02-28 08:12:05 +0000

    Executable file for the Docker and Kubernetes installation

--- kubectl.sh
+++ kubectl.sh
@@ -1,14 +1,6 @@
 sudo apt-get update -y
-sudo apt-get install -y apt-transport-https ca-certificates curl
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
-kubectl version --short
-kubeadm version --output=yaml
 clear
-sleep 10
+sleep 5
 count=$(apt list --installed | grep docker | wc -l)
 if [ $count -eq 1 ]
 then
@@ -20,4 +12,16 @@ else
 	docker --version
 fi
 sudo docker info
+sleep 10
+clear
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /et>
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
+sleep 2
+kubectl version --short
+sleep 2
+kubeadm version --output=yaml
 
