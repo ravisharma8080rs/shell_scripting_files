7ddeb8706a2835e950e032a79149dc8ead2ea86a
Ubuntu <ubuntu@ip-120-0-1-156.ap-south-1.compute.internal>
2024-02-28 08:35:01 +0000

    Executable file for the Docker and Kubernetes installation

--- kubectl.sh
+++ kubectl.sh
@@ -4,16 +4,20 @@ sleep 2
 count=$(apt list --installed | grep docker | wc -l)
 if [ $count -eq 1 ]
 then
+	echo "Docker already installed !"
         docker --version
 else
+	echo "Docker installation has been start !"
+	sleep 2
         sudo apt-get install docker.io -y
 	sudo systemctl start docker
 	sudo systemctl enable docker
 	docker --version
 fi
 sudo docker info >> dockerinfo.txt
-sleep 2
 clear
+echo "kubernets installation has been start !"
+sleep 2
 sudo apt-get install -y apt-transport-https ca-certificates curl
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -21,7 +25,8 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
 sleep 2
-kubectl version --short
+kubeadm version --output=yaml >> kubeadminfo.txt
+clear
 sleep 2
-kubeadm version --output=yaml
-
+echo "Installation of docker and kubernets has been completed !"
+sleep 5
