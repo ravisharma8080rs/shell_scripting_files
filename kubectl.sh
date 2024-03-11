db953a7e6535163fdfa57fdda519be1774778d40
U-NEXSUS\Ravi.Sharma <Ravi.Sharma@Nexsus-TechLap3.nexsus.local>
2024-03-11 12:43:45 +0530

    Executable file for the Docker and Kubernetes installation

--- kubectl.sh
+++ kubectl.sh
@@ -20,12 +20,16 @@ echo "kubernets installation has been start !"
 sleep 2
 sudo apt-get install -y apt-transport-https ca-certificates curl
 echo "Downloading the public signing key for the Kubernetes package repositories"
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.20/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update -y
-sudo apt-get install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
 sleep 2
+clear
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.25/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.25/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -y
+sudo apt-get install kubectl kubeadm kubelet -y
+sleep 2
+kubectl version --client --output=yaml >> kubectlinfo.txt
 kubeadm version --output=yaml >> kubeadminfo.txt
+kubelet --version
 clear
 sleep 2
 echo "Installation of docker and kubernets has been completed !"
